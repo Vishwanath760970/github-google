2618f6106f59848c0d52b0de39f18dd0a2ad3c3f
vishwanath <vishwanath0c1@gmail.com>
2020-09-05 17:41:03 +0530

    alert component for empty search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 import axios from 'axios';
 
@@ -12,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   //search github users, by getting this text parameter value from search.js input field, by passing up this prop
@@ -32,16 +34,25 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  //set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
 
           <Users loading={loading} users={users} />
