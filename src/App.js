4ffaefe98cabd47a37b6a0e844516e254dba69fe
vishwanath <vishwanath0c1@gmail.com>
2020-09-05 14:30:08 +0530

    implement clear button feature in search js

--- src/App.js
+++ src/App.js
@@ -24,13 +24,25 @@ class App extends Component {
       users: res.data.items,
       loading: false,
     });
+    console.log(res.data.items);
   };
+
+  //clear users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
 
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
